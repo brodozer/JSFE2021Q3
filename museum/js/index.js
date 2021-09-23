1346284a7ee53eb7f26d6b76f594757ae61390b8
Aleksandr Perepelitsa <perepelitsa.alex@gmail.com>
2021-09-23 10:54:59 +0300

    feat: add header, section welcone, tour pages

--- museum/js/index.js
+++ museum/js/index.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const swiperWelcome = new Swiper(".swiper-welcome", {
+	loop: true,
+	slidesPerView: 1,
+	pagination: {
+		el: ".pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".button-next",
+		prevEl: ".button-prev",
+	},
+	on: {
+		slideChange: function () {},
+	},
+});
+
+const current = document.querySelector(".swiper-welcome .current");
+
+swiperWelcome.on("slideChange", function () {
+	let index = swiperWelcome.realIndex;
+	current.innerHTML = `0${++index}`;
+});
